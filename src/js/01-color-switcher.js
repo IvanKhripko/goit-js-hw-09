318691ed7b47c5f5a00c480b675689e01490a1a4
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2022-02-05 23:03:30 +0200

    Cделал 'color-switcher'.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
 import '../css/common.css';
+
+const bodyEl = document.body;
+// console.log(bodyEl);
+
+const btnStartEl = document.querySelector('button');
+// console.log(btnStartEl);
+
+const btnStopEl = btnStartEl.nextElementSibling;
+// console.log(btnStopEl);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+
+btnStartEl.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+
+    bodyEl.style.backgroundColor = color;
+
+    btnStartEl.setAttribute('disabled', 'disabled');
+    btnStopEl.removeAttribute('disabled');
+  }, 1000);
+});
+
+btnStopEl.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStopEl.setAttribute('disabled', 'disabled');
+  btnStartEl.removeAttribute('disabled');
+});
