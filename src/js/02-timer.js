bf532f799abca336fb87e8d2cbb0b5aa073867ce
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2022-02-06 00:06:09 +0200

    Подключил библиотеку flatpickr

---
+++ src/js/02-timer.js
@@ -0,0 +1,15 @@
+import '../css/common.css';
+import 'flatpickr/dist/flatpickr.min.css';
+import flatpickr from 'flatpickr';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', options);
